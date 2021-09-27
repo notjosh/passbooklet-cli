7a11ded7136e2582cbbd1b3ddcc33e1595466964
Joshua May <notjosh@gmail.com>
2021-09-27 15:38:11 +0200

    [wip] started CMS splitting

---
+++ src/cms/index.ts
@@ -0,0 +1,5 @@
+// very simplistic implementation of CMS (https://datatracker.ietf.org/doc/html/rfc5652)
+// for our purposes.
+//
+// it's missing plenty of things (i.e. enveloped data), but it should be reasonably useful
+// for signing/verifying.
